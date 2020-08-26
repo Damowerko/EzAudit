e4ff34820844c8f37354c7f333275fa24aa40a27
Damian Owerko <damowerko@gmail.com>
2020-08-25 22:52:13 -0400

    All endpoints tested

--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,18 @@
 const http = require("http");
 const express = require("express");
-const api = require("api");
-
+const api = require("./api");
+const mongoose = require("mongoose");
 const ConferenceServer = require("../conference/ConferenceServer");
 
+const DB_URI =
+  "mongodb+srv://admin:pQpbNv6tJs30r1Oq@ezaudit.jvmwz.mongodb.net/ezaudit?retryWrites=true&w=majority";
 const PORT = 4000;
 
 const app = express();
 const server = http.createServer(app);
+mongoose.connect(DB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use("/api", api);
-const conferenceServer = new ConferenceServer(server);
+new ConferenceServer(server);
 
 server.listen(PORT, () => console.log(`Listening on *:${PORT}`));

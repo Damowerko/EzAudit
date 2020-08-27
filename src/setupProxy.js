f5135415fe3f75239072c6433b04eabe50a525f5
Damian Owerko <damowerko@gmail.com>
2020-08-27 18:52:09 -0400

    Enable websocket proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,16 +2,14 @@ const {createProxyMiddleware} = require("http-proxy-middleware");
 const config = require("./config")
 
 module.exports = function (app) {
-  //app.use(cors());
-
   app.use(
     "/socket.io",
     createProxyMiddleware({
       target: `http://${config.ip}:4000`,
       changeOrigin: true,
+      ws: true
     }),
   );
-
   app.use(
     "/api",
     createProxyMiddleware({

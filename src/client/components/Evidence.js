27ebf19c076c492ed9f09b633d767124bbabe6db
You Song Hou <yousonghou@yahoo.com>
2020-08-27 20:23:24 -0400

    allow the user to download the file

--- src/client/components/Evidence.js
+++ src/client/components/Evidence.js
@@ -3,6 +3,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+
+import GetAppIcon from '@material-ui/icons/GetApp';
 
 import Document from '../images/document.png';
 
@@ -13,6 +16,9 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     padding: '1rem',
     textAlign: 'center',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
     color: theme.palette.text.secondary,
   },
 }));
@@ -28,7 +34,15 @@ export default function Evidence({ files }) {
             return(
               <Grid item xs={12} sm={6} md={4} lg={3} key={file._id}>
                 <Paper className={classes.paper}>
-                    <Typography align="left">{file.originalname}</Typography>
+                    <Typography variant="h6" align="left">
+                      {file.originalname}
+                    </Typography>
+                    <a href={'http://localhost:4000/api/file/' + file._id} target="_blank" download>
+                      <IconButton>
+                        <GetAppIcon />
+                      </IconButton>
+                    </a>
+                    
                 </Paper>
             </Grid>
             )
